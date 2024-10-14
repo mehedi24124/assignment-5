f1ba303c5a071ec7ac46c920705ce5a4fad6fc2b
Mehedi Hasan <mehedi24124@gmail.com>
2024-10-14 22:21:08 +0600

    blog section done

---
+++ create.js
@@ -0,0 +1,13 @@
+//  banner section two button one click after another click
+function toggleInfo(infoId) {
+    const info1 = document.getElementById('info1');
+    const info2 = document.getElementById('info2');
+
+    if (infoId === 'info1') {
+        info1.style.display = 'block';
+        info2.style.display = 'none';
+    } else {
+        info1.style.display = 'none';
+        info2.style.display = 'block';
+    }
+}
